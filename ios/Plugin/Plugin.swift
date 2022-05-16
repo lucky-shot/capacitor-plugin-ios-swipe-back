7c19dca438c6f07053cc3e39f593b28c0001280e
Luan Trasel <luantrasel77@gmail.com>
2022-05-16 16:55:47 -0300

    fixed iOS error

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,14 +9,14 @@ import Capacitor
 public class IosSwipeBack: CAPPlugin {
     
     @objc func enable(_ call: CAPPluginCall) {
-        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = true;
+        self.bridge?.getWebView()?.allowsBackForwardNavigationGestures = true;
         call.resolve([
             "error": 0
             ]);
     }
     
     @objc func disable(_ call: CAPPluginCall) {
-        self.bridge.getWebView()?.allowsBackForwardNavigationGestures = false;
+        self.bridge?.getWebView()?.allowsBackForwardNavigationGestures = false;
         call.resolve([
             "error": 0
             ]);
